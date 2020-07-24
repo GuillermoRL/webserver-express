7c77b42ca49c1f038149d5fe09a6b4dbb9dbb844
Guillermo <guillermo.rafael1@gmail.com>
2020-07-24 13:51:33 -0600

    primer commit

---
+++ app-old.js
@@ -0,0 +1,17 @@
+// Iniciar un webservice
+const http = require('http');
+
+http.createServer( (req, res) => {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        let salida = {
+            nombre: 'Guillermo',
+            Edad: 28,
+            url: req.url
+        }
+        res.write(JSON.stringify(salida));
+        // res.write('Hola Mundo');
+        res.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080')
